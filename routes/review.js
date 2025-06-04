c54bbfa96e6878ff5fbcabbb8c24fbd091e25b4b
0xashishtiwari <ashishinrewa@gmail.com>
2025-06-04 16:31:44 +0530

    added controller dor review route

--- routes/review.js
+++ routes/review.js
@@ -6,34 +6,19 @@ const {reviewSchema} = require('../schema');
 const wrapAsync = require('../utils/WrapAsync');
 const Listing = require('../models/listing');
 const { validateReview, isLoggedIn , isreviewAuthor} = require('../middleware');
+const reviewController = require('../controllers/reviews')
+
+
 
 //--------------Review POST Route------------------------
 
-router.post('/' ,isLoggedIn ,validateReview ,  wrapAsync( async(req , res)=>{
-   
-    let {id} = req.params
-    let listing =  await Listing.findById(id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    console.log(newReview);
-    listing.reviews.push(newReview);
+router.post('/' ,isLoggedIn ,validateReview ,  wrapAsync(reviewController.createReview ));
 
-    await newReview.save();
-    await listing.save();
-    req.flash('success'  , 'New Review Added!')
-    res.redirect(`/listings/${id}`);
-}));
 
 
 //------------DELETE Review Route----------------------
 
-router.delete('/:reviewId' ,isLoggedIn, isreviewAuthor,wrapAsync(async(req ,res)=>{
-    let {id , reviewId} = req.params; 
-    Listing.findByIdAndUpdate(id , {$pull : {reviews : reviewId}}); // removing data from the listing review array using $pull
-    await Review.findByIdAndDelete(reviewId);
-     req.flash('success'  , ' Review Deleted!')
-    res.redirect(`/listings/${id}`);
-}));
+router.delete('/:reviewId' ,isLoggedIn, isreviewAuthor,wrapAsync(reviewController.destroyReview));
 
 
 module.exports = router;
